1325681a96079576eced975a7f2f23ee05481367
Viktorjs <92879968+Viktorjs@users.noreply.github.com>
2021-10-20 18:58:58 +0200

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,18 @@
+
+// creates abstract class
+public abstract class CoffeeAbstract {
+	// variables for the subclasses to inherit
+	int high;
+	int low;
+	int RandomEnergy;
+
+	// setters and getters for random energy value
+	public int getRandomEnergy() {
+		return RandomEnergy;
+	}
+
+	public void setRandomEnergy(int randomEnergy) {
+		RandomEnergy = randomEnergy;
+	}
+
+}
